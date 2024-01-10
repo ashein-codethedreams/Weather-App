86525d523f04c292c77eec0a42d15302765b34eb
ashein-codethedreams <heroglasses155@gmail.com>
2024-01-10 16:29:06 +0630

    het weather detail by name with apikey

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import CurrentWeather from "./components/CurrentWeather";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p className="text-lg ">Current Weather</p>
+      <br />
+      <CurrentWeather />
+    </div>
+  );
 }
 
 export default App;
